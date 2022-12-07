d3db31766b11313939f5328f6545ae6bfc4ad34a
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2022-12-06 23:27:23 -0300

    corrigido erro nas rotas e na pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-
+  resources :menu
   resources :pacientes do
       resources :enderecos
-      resources :consulta
   end
-  resources :menus
+  resources :consulta
   resources :medicos
 
     
