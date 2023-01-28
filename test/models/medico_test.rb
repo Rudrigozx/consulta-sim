80cf5c4763f81b8d2bfd7ba5ad154ff1cb7f3448
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-01-27 23:04:50 -0300

    teste de model de Medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Salvar o medico com crm invalido" do
+    @medico = Medico.new nome:"Ricardo",
+                         cpf:"12378945652",
+                         email:"ricardo@gmail.com",
+                         especialidade:"cardiologista",
+                         crm:'arroz'
+
+    assert_not @medico.save
+  end
+
+  test "Salvar o medico sem especialidade" do
+    @medico = Medico.new nome:"Ricardo",
+                         cpf:"12378945652",
+                         email:"ricardo@gmail.com",
+                         especialidade:"",
+                         crm:123456
+
+    assert_not @medico.save
+  end
 end
