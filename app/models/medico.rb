6d71cacb4492f458639f76be6648a018f7dd5ec0
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-01-30 01:35:03 -0300

    correção nas validações

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,9 +1,9 @@
 class Medico < ApplicationRecord
     has_many :consultum, dependent: :destroy
     has_many :pacientes, through: :consultum
-    validates :nome, length: { minimum: 4 }, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    validates :nome, length: { minimum: 4 }, presence: true
     validates :cpf, length: { is: 11 }, presence: true, numericality: { only_integer: true }, uniqueness: true
     validates :email, length: { minimum: 11 }, presence: true, uniqueness: true
-    validates :especialidade, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, presence: true, length: { in: 4..20 }
+    validates :especialidade, presence: true, length: { in: 4..20 }
     validates :crm, numericality: { only_integer: true }, presence: true, uniqueness: true
 end
